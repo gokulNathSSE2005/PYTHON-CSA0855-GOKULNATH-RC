be4b56adf589810db496512ffd22c4a11ea9e179
gokulNathSSE2005 <greengg2005@gmail.com>
2024-12-14 08:45:44 +0530

    day 3 Saturday 7.12.2024

---
+++ matrix multiplication.py
@@ -0,0 +1,16 @@
+X=[[1,2],
+   [5,3]]
+Y=[[2,3],
+   [4,1]]
+result=[[0,0],
+        [0,0]]
+
+for i in range(len(X)):
+  
+   for j in range(len(Y[0])):
+       
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
